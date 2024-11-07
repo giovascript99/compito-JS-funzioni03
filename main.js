2da34fa5687a22c57ab3b116cb8b599d6df127cc
Giovanni Perniola <gioperniola1999@gmail.com>
2024-11-07 19:00:06 +0100

    selfwork corretto

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-function contaCifre(numero) {
-    if (numero > 0 && numero < 10000) {
-        return numero.toString().length;
-
-    } else {
-        return "Numero troppo grande";
+function contaCifre(num){
+    if (num < 10){
+        return `1 cifra`
+    }else if (num < 100){
+        return `2 cifre`
+    }else if (num < 1000){
+        return `3 cifre`
+    }else if (num < 10000){
+        return `4 cifre`
+    }else{
+        return `numero troppo grande`
     }
 }
 
 console.log(contaCifre(9));
+console.log(contaCifre(99));
 console.log(contaCifre(999));
 console.log(contaCifre(9999));
 console.log(contaCifre(99999));
+
